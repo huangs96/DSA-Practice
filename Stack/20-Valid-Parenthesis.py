2ccf396a5d414c384770a0a8fb2ab93c807a1d96
Stephen <stephen_1996@live.ca>
2023-02-11 16:01:03 -0800

    wrote solution for py 20 valid parenthesis

---
+++ Stack/20-Valid-Parenthesis.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    # @return a boolean
+    def isValid(self, s):
+        stack, lookup = [], {"(": ")", "{": "}", "[": "]"}
+        for parenthese in s:
+            if parenthese in lookup:
+                stack.append(parenthese)
+            elif len(stack) == 0 or lookup[stack.pop()] != parenthese:
+                return False
+        return len(stack) == 0
+
+solution = Solution()
+print(solution.isValid("()[]{}"))
+print(solution.isValid("(]"))
+print(solution.isValid("()"))
