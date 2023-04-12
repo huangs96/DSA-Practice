11f3e650df3e3e0247de49cfd7a38ebceef8dbdf
Stephen <stephen_1996@live.ca>
2023-04-11 23:51:05 -0700

    completed on Odd Occurances problem

--- Codility Practice/oddOccurances.js
+++ Codility Practice/oddOccurances.js
@@ -2,5 +2,23 @@
 // console.log('this is a debug message');
 
 function solution(A) {
-  // Implement your solution here
-}
+  if (A.length < 1) {
+      return 0;
+  };
+
+  const hashmap = {};
+  for (let x=0; x<A.length; x++) {
+      if (hashmap[A[x]]) {
+          hashmap[A[x]] = hashmap[A[x]] + 1;
+      } else {
+          hashmap[A[x]] = 1;
+      };
+  };
+
+  const keys = Object.keys(hashmap);
+  for (let key of keys) {
+      if (hashmap[key] % 2 !== 0) {
+          return parseInt(key);
+      };
+  };
+};
