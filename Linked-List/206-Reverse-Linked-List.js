bcd3ece1107b1b5ae5d9b32ad4d5101ad4f64cc0
Stephen <stephen_1996@live.ca>
2023-04-13 14:30:35 -0700

    changed folder to sort&search

--- Linked-List/206-Reverse-Linked-List.js
+++ Linked-List/206-Reverse-Linked-List.js
@@ -10,6 +10,8 @@
  * @return {ListNode}
  */
 
+let head = [1, 2, 3, 4, 5];
+
  var reverseList = function(head) {
   if (head === null) return head;
 
@@ -17,7 +19,13 @@
   let prevNode = null;
   let nextNode = null;
 
-  while (currentNode) {
-      nextNode = currentNode.next;
-  }
+  // while (currentNode) {
+  //     nextNode = currentNode.next;
+  //     prevNode = nextNode;
+  //     console.log(currentNode);
+  // };
+
+  console.log('currentNode', head.next);
 };
+
+console.log('reverse list', reverseList(head));
