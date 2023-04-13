5f50042afcb8cb2c0dd9106b237270e13d7b7ca3
Stephen <stephen_1996@live.ca>
2023-04-13 16:04:57 -0700

    completed BubbleSort algorithm for js

--- Search-Sort/BubbleSort/BubbleSort.js
+++ Search-Sort/BubbleSort/BubbleSort.js
@@ -4,19 +4,20 @@ const bubbleSort = (arr) => {
   for (let x=0; x<arr.length; x++) {
     for (let y=0; y<(arr.length - x - 1); y++) {
       //y is subtracting 1 from end of arr every iteration
-      // console.log('y', arr[y]);
-      // console.log('y+1', arr[y+1]);
+      // if arr[y] is greater than the index next to it, bubble up arr[y] 
       if (arr[y] > arr[y+1]) {
+        //storing first arr[y] as temp
         let temp = arr[y];
-        console.log('temp1', temp);
+        //changing arr[y] to arr[y+1]
         arr[y] = arr[y + 1];
-        console.log('arr[y]line 12', arr[y]);
+        //using stored value in temp to change arr[y+1] to previous value of arr[y]
         arr[y + 1] = temp;
-        console.log('arr[y+1] line 15', arr[y+1]);
+        // console.log('arr[y+1] line 15', arr[y+1]);
       }
     };
-
+    console.log('arr1', test.arr1);
   };
+  console.log('arr2', test.arr1);
 };
 
 console.log(bubbleSort(test.arr1));
