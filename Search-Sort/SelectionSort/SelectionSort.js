ea8cc235ec5fb7942f5b440f115565f8f66f02ac
Stephen <stephen_1996@live.ca>
2023-04-13 15:03:51 -0700

    completed Selection Sort with notes

--- Search-Sort/SelectionSort/SelectionSort.js
+++ Search-Sort/SelectionSort/SelectionSort.js
@@ -3,21 +3,26 @@ const arr = [77,31,28,21,14,41,15,18,49,10];
 const selectionSort = (arr) => {
   //initiate first index number as smallest number
   for (let x=0; x<arr.length; x++) {
+    //x starts at 0
     let minimum = x;
     //second loop to compare elements next to arr[x]
     for (let y=x+1; y<arr.length; y++) {
+      //y starts at 1
       if (arr[y] < arr[minimum]) {
+        console.log('y', [arr[y]]);
+        console.log('min', [arr[minimum]]);
+        //if arr[y] is less than minimum, y index becomes the new minimum index. From there, the next iteration will be minimum + 1 to check if the element next to it is greater or smaller
         minimum = y;
-        //minimum index is now acheived through first iteration
       };
     };
 
     if (minimum !== x) {
+      //if minimum index has changed, the two elements will be swapped because the initial minimum is no longer the smallest element between the two
       [arr[x], arr[minimum]] = [arr[minimum], arr[x]];
-      console.log('swap check', arr)
     }
   };
-  console.log('swap check22222', arr);
+  
+  return arr;
 };
 
 console.log(selectionSort(arr));
