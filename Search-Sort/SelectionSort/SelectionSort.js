4005ee88d7903fb67043444e6c21a255fe9989fc
Stephen <stephen_1996@live.ca>
2023-04-13 15:22:37 -0700

    created testVariables file to test for sorting algorithms

--- Search-Sort/SelectionSort/SelectionSort.js
+++ Search-Sort/SelectionSort/SelectionSort.js
@@ -1,4 +1,4 @@
-const arr = [77,31,28,21,14,41,15,18,49,10];
+const test = require('../testVariables');
 
 const selectionSort = (arr) => {
   //initiate first index number as smallest number
@@ -9,8 +9,6 @@ const selectionSort = (arr) => {
     for (let y=x+1; y<arr.length; y++) {
       //y starts at 1
       if (arr[y] < arr[minimum]) {
-        console.log('y', [arr[y]]);
-        console.log('min', [arr[minimum]]);
         //if arr[y] is less than minimum, y index becomes the new minimum index. From there, the next iteration will be minimum + 1 to check if the element next to it is greater or smaller
         minimum = y;
       };
@@ -25,4 +23,4 @@ const selectionSort = (arr) => {
   return arr;
 };
 
-console.log(selectionSort(arr));
+console.log(selectionSort(test.arr1));
