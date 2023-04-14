9aad896ee1c073a79fe7d5e897acbdc3caa20524
Stephen <stephen_1996@live.ca>
2023-04-13 23:02:35 -0700

    bubble sort with notes completed

--- Search-Sort/BubbleSort/BubbleSort.js
+++ Search-Sort/BubbleSort/BubbleSort.js
@@ -2,10 +2,12 @@ const test = require('../testVariables');
 
 const bubbleSort = (arr) => {
   for (let x=0; x<arr.length; x++) {
+    //here we do arr.length - x - 1 because we don't want to go over the sorted elements of the array already. E.G., if we have already sorted arr[0], we can take that off the array for sorting and iterating
     for (let y=0; y<(arr.length - x - 1); y++) {
       //y is subtracting 1 from end of arr every iteration
       // if arr[y] is greater than the index next to it, bubble up arr[y] 
       if (arr[y] > arr[y+1]) {
+        console.log('arry', arr[y]);
         //storing first arr[y] as temp
         let temp = arr[y];
         //changing arr[y] to arr[y+1]
