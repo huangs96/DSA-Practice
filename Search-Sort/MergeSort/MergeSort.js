591dba3781f72e8410f4a801b9ab0fd963250dbe
Stephen <stephen_1996@live.ca>
2023-04-15 15:49:43 -0700

    added second test data to further understand recursion

--- Search-Sort/MergeSort/MergeSort.js
+++ Search-Sort/MergeSort/MergeSort.js
@@ -1,35 +1,37 @@
 const test = require('../testVariables');
+const test2 = [6,4,2,3,1,5];
 
 const merge = (left, right) => {
   let arr = [];
 
   while (left.length && right.length) {
+    console.log('left in while', left)
+    console.log('right in while', right)
     if (left[0] < right[0]) {
       arr.push(left.shift());
     } else {
       arr.push(right.shift());
     };
-    console.log('arr2 in whileloop', arr);
+    console.log('arr in while', arr);
   };
-
+  console.log('arr22222', arr)
+  console.log('left22222', left)
+  console.log('right22222', right)
   return [...arr, ...left, ...right];
 };
 
 const mergeSort = (arr) => {
-  console.log('arr', arr);
+  console.log('initial arr', arr);
   const half = arr.length / 2;
 
   //terminate recursion
   if (arr.length < 2) {
-    console.log('here', arr);
+    console.log('base case met', arr);
     return arr;
   };
 
   const left = arr.splice(0, half);
-  console.log('left', left);
-  console.log('right', arr);
-  console.log('merge', merge(mergeSort(left), mergeSort(arr)));
   return merge(mergeSort(left), mergeSort(arr));
 };
 
-console.log(mergeSort(test.arr1));
+console.log(mergeSort(test2));
