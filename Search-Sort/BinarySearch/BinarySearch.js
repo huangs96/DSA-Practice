cd4e5205ad1af648e669db5c6505987bb8959d35
Stephen <stephen_1996@live.ca>
2023-04-15 16:42:21 -0700

    initiating mid point in sorted arr

--- Search-Sort/BinarySearch/BinarySearch.js
+++ Search-Sort/BinarySearch/BinarySearch.js
@@ -1,2 +1,11 @@
 const test = require('../testVariables');
 
+let recursiveBinarySearch = (arr, x, start, end) => {
+  //if start index is greater than end index, arr is invalid
+  if (start > end) {
+    return false;
+  };
+
+  let mid = Math.floor((start + end) / 2);
+  console.log('mid', mid);
+}
