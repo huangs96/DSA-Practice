2327dd3ee38d370b428848979640f6ef1d884cbe
Stephen <stephen_1996@live.ca>
2023-04-15 15:31:19 -0700

    deleted all console logs for

--- Recursion/RecursionPractice1.js
+++ Recursion/RecursionPractice1.js
@@ -1,12 +1,8 @@
 const factorial = (n) => {
-  console.log('initial n', n);
   if (n == 1 || n == 0) {
-    console.log('basecase met', n);
     return 1;
   } else {
-    console.log('base case not met', n)
-    console.log('base case not met with next factorial', n - 1);
     return n * factorial(n-1);
   };
 };
-console.log('factorial', factorial(4));
+console.log('factorial', factorial(10));
